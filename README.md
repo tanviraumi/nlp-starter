7f22732b4a3efa614892dc98e04ff0d15a75f0a0
Tanvir Aumi <tanvir@cs.washington.edu>
2022-04-22 17:30:19 -0700

    Simple trainer

--- README.md
+++ README.md
@@ -8,23 +8,32 @@ source .env/bin/activate
 
 ## Install required packaages
 
-Install Huggingface transformes
-```
-pip3 install "transformers[sentencepiece]"
-```
-
 Install torch based on your [architecture](https://pytorch.org/get-started/locally/)
+
 ```
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 ```
 
-Check whether architecture and Cuda versions are installed properly
+Usually the command should work but in my case, Python version 3.10 was incompatible with Cuda so I had to install the nightly build instead.
+
+```
+pip3 install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
+```
+
+Finally Check whether architecture and Cuda versions are installed properly
 
 ```
 python3 cuda.py
 ```
 
-Now install Huggingface datasets
+Now install Huggingface transformes and datasets
 ```
+pip3 install "transformers[sentencepiece]"
 pip3 install datasets
+```
+
+Some projects (train.py) also requires scipy and sklearn
+
+```
+pip3 install scipy sklearn
 ```
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
-from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding, TrainingArguments
+from datasets import load_dataset, load_metric
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding, TrainingArguments, Trainer
 
 # Pre process the data (pair of sentences)
 raw_datasets = load_dataset("glue", "mrpc")
@@ -15,7 +16,33 @@ data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 # Instantiate the trainer. TrainingArguments class that will contain all the hyperparameters
 # the Trainer will use for training and evaluation
-training_args = TrainingArguments("test-trainer")
+training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
 
 # Now we need to define our model
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+# Define a compute metrics
+def compute_metrics(eval_preds):
+    metric = load_metric("glue", "mrpc")
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+# Once we have our model, we can define a Trainer by passing it all the objects constructed up to now — 
+# the model, the training_args, the training and validation datasets, our data_collator, and our tokenizer
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+# Finally its time to train the model, Watch out for the GPUs to crank up.
+trainer.train()
+
+# Look at some predictions
+predictions = trainer.predict(tokenized_datasets["validation"])
+print(predictions.predictions.shape, predictions.label_ids.shape)
